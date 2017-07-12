b0e5d6555cfca8879b27e3593b567273aea495a6
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-12 15:01:37 -0700

    php

--- backend.php
+++ backend.php
@@ -29,22 +29,24 @@ function get_Projects($category) {
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            print "<a><div class=\"category_Block\">";
+            if($row['category'] == $category) {
+                print "<a><div class=\"category_Block\">";
 
-            $dir = "projects/" .  $row['category'] . "/" . $row['id']; // current directory
-            $first_Image_Dir = return_First_File_Name($dir);
+                $dir = "projects/" .  $row['category'] . "/" . $row['id']; // current directory
+                $first_Image_Dir = return_First_File_Name($dir);
 
 
-            print "<img src=\"$first_Image_Dir\">";
+                print "<img src=\"$first_Image_Dir\">";
 
-            print "<div class=\"project_Block_Overlay\">";
-            print "<h1>" . $row['project_name'] . "</h1>";
-            print "<p>" . $row['project_location'] . "</p>";
+                print "<div class=\"project_Block_Overlay\">";
+                print "<h1>" . $row['project_name'] . "</h1>";
+                print "<p>" . $row['project_location'] . "</p>";
 
-            print "</div>"; // END overlay
-            print "</div></a>"; // END block
+                print "</div>"; // END overlay
+                print "</div></a>"; // END block
 
-            ++ $fade_in_time;
+                ++ $fade_in_time;
+            }
         }
     }
     $conn->close();
