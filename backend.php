6f0357fda5fdf0bb805cd74f794d23b98ba9d4fb
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-12 15:56:22 -0700

    php

--- backend.php
+++ backend.php
@@ -14,7 +14,7 @@ function get_Categories(){
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $name = $row['name'];
-            print "<div class=\"category_Block\"><a onclick='getProjects(\"$name\")'>$name</a></div>";
+            print "<div class=\"category_Block\"><a onclick='getProjects('$name')'>$name</a></div>";
         }
     }
     $conn->close();
