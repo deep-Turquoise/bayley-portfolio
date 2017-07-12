34a824ad7e8f8850cbb8b864bd1f52465a7240b3
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-12 14:20:07 -0700

    data

--- backend.php
+++ backend.php
@@ -1,6 +1,17 @@
 <?php
 
 function get_Categories(){
+    $conn = new mysqli("localhost", "root", "airpolo3", "intranet_Bayley");
+    if ($conn->connect_error) { print "Database Connection Error"; }
+    $sql = "SELECT * FROM category";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            print "<a><div class=\"category_Block\">" . row['name'] ."</div></a>";
+        }
+    }
+    $conn->close();
 
 }
 
