cb4edae0e79a55a0aa1768d3d38e7b06162a1a6b
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-12 16:48:44 -0700

    test

--- backend.php
+++ backend.php
@@ -8,21 +8,31 @@ function return_First_File_Name($dir) {
 function openModal($category, $id) {
     $conn = new mysqli("localhost", "root", "airpolo3", "intranet_Bayley");
     if ($conn->connect_error) { print "Database Connection Error"; }
-    $sql = "SELECT * FROM categories";
+    $sql = "SELECT * FROM projects";
     $result = $conn->query($sql);
 
-    //Just so we can make featured first
-    print "<a onclick=\"getProjects('Featured')\"><div class=\"category_Block\">Featured</div></a>";
+    $delay = 0.1; // used for increment the fade in time...
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            $name = $row['name'];
-            if($name != "Featured") {
-                print "<a onclick=\"getProjects('$name')\"><div class=\"category_Block\">$name</div></a>";
+            if($row['category'] == $category && $row['id'] == $id) {
+                print "<div class=\"modal_content\">";
+                $dir = "projects/" .  $category . "/" . $id; // current directory
+                $first_Image_Dir = $dir . "/" . return_First_File_Name($dir);
+
+                print "<img src=\"$first_Image_Dir\">";
+
+                print "<div class=\"modal_image_overlay\">";
+                print "<h1>" . $row['project_name'] . "</h1>";
+                print "<p>" . $row['project_location'] . "</p>";
+
+                print "</div>"; // END overlay
+                print "</a></div>"; // END block
+
+                $delay += 0.15;
             }
         }
     }
-    $conn->close();
 }
 
 function get_Categories(){
