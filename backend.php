3368ea90021ef4ef129845adb5a7ebe23c5ba1a1
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-19 13:19:10 -0700

    fixes

--- backend.php
+++ backend.php
@@ -9,32 +9,34 @@ function changeImage($instructions, $image_URL) {
     $cur_Image_Name = substr(strrchr($chunkB, "/"), 1);
 
     $arr_dir = scandir("projects/$cur_Category/$cur_Id/");
-    unset($arr_dir[0]); // removing "."
-    unset($arr_dir[1]); // removing ".."
 
     for($x = 0; $x < count($arr_dir); ++$x) {
-        if($arr_dir[$x] == $cur_Image_Name) {
+        if($arr_dir[$x] == $cur_Image_Name && count($arr_dir) < 3) {
             if($instructions == "nextImage") {
                 $next_Num = $x + 1;
-                if($next_Num < (count($arr_dir)-1)) {
+                if($next_Num < count($arr_dir)) {
                     $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[$next_Num];
                     print $new_image;
                 }
                 else {
-                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[0];
+                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[2];
                     print $new_image;
                 }
             }
             else {
-                if($x == 0 && count($arr_dir) != 1) {
-                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[$x+1];
+                if($x > 2) {
+                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[$x-1];
                     print $new_image;
                 }
                 else {
-                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[0];
+                    $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[2];
                     print $new_image;
                 }
             }
+        }
+        else {
+            $new_image = "projects/$cur_Category/$cur_Id/" . $arr_dir[2];
+            print $new_image;
         }
     }
 }
